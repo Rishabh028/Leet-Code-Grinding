499752ae213ec3e49f3c8340fe4a6ecd5079833d
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-08-01 02:21:16 +0530

    Time: 3 ms (16.65%), Space: 9.5 MB (50.88%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -4,14 +4,14 @@ public:
     {
         int a = 0, b = 0;
         int check = '0';
-        for(int i = 0; i<s.size(); ++i) //start on 0
+        for(int i = 0; i<s.size(); ++i) 
         {
             if(s[i] != check) a++;
             if(check == '0') check = '1';
             else check = '0';
         }
         check = '1';
-        for(int i = 0; i<s.size(); ++i) //start on 1
+        for(int i = 0; i<s.size(); ++i)
         {
             if(s[i] != check) b++;
             if(check == '0') check = '1';
