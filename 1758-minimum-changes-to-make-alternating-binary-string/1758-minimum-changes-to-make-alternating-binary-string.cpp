df7d0de4a3c0873dee6a5a0052b479a573140ff1
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-08-01 02:21:32 +0530

    Time: 3 ms (16.65%), Space: 9.3 MB (77.95%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int minOperations(string s) 
-    {
-        int a = 0, b = 0;
-        int check = '0';
-        for(int i = 0; i<s.size(); ++i) 
-        {
-            if(s[i] != check) a++;
-            if(check == '0') check = '1';
-            else check = '0';
+    int minOperations(string s) {
+        int way1=0,way2=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                if(s[i]!='0') way1++;
+            }else{
+                if(s[i]!='1') way1++;
+            }
         }
-        check = '1';
-        for(int i = 0; i<s.size(); ++i)
-        {
-            if(s[i] != check) b++;
-            if(check == '0') check = '1';
-            else check = '0';
-        }
-
-        return min(a,b);
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                if(s[i]!='1') way2++;
+            }else{
+                if(s[i]!='0') way2++;
+            }
+        }return min(way1,way2);
     }
 };
