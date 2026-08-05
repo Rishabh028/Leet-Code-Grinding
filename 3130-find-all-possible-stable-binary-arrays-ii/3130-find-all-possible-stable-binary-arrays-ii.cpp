7f7b7a22d9ad496569b8d2e0daec618d4d9ecd57
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-08-05 23:53:56 +0530

    Time: 179 ms (60.69%), Space: 17.1 MB (97.8%) - LeetHub

--- 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
@@ -1,21 +1,25 @@
-int mod=1e9+7;
-int dp[1001][1001][2];
-int pre[1001][1001][2];
 class Solution {
 public:
-
-int numberOfStableArrays(int zero, int one, int limit) {
-    dp[0][0][0]=1;dp[0][0][1]=1;
-    pre[0][0][0]=1;pre[0][0][1]=1;
-    for (int i=0;i<=zero;i++){
-        for (int j=0;j<=one;j++){
-            if (i==0 && j==0) continue;
-            if (j>0) dp[i][j][0]=(pre[i][j-1][1]-((j-min(limit,j)-1>=0)? pre[i][j-min(limit,j)-1][1]:0)+mod)%mod;
-            if (i>0) dp[i][j][1]=(pre[i-1][j][0]-((i-min(i,limit)-1>=0)?pre[i-min(i,limit)-1][j][0]:0)+mod)%mod;
-            pre[i][j][0]=(((i>0)? pre[i-1][j][0]:0)+dp[i][j][0])%mod;
-            pre[i][j][1]=(((j>0)? pre[i][j-1][1]:0)+dp[i][j][1])%mod;
+    int dp[1001][1001][2];
+    int numberOfStableArrays(int zero, int one, int limit) {
+        for (int z = 0; z <= zero && z <= limit; z++) {
+            dp[z][0][0] = 1;
         }
-    }
-    return (dp[zero][one][0]+dp[zero][one][1])%mod;
+        for (int o = 0; o <= one && o <= limit; o++) {
+            dp[0][o][1] = 1;
+        }
+        for (int z = 1; z <= zero; z++) {
+            for (int o = 1; o <= one; o++) {
+                dp[z][o][0] = (dp[z-1][o][0] + dp[z-1][o][1]) % 1000000007;
+                if (z > limit) {
+                    dp[z][o][0] = (dp[z][o][0] + 1000000007 - dp[z-limit-1][o][1]) % 1000000007;
+                }
+                dp[z][o][1] = (dp[z][o-1][0] + dp[z][o-1][1]) % 1000000007;
+                if (o > limit) {
+                    dp[z][o][1] = (dp[z][o][1] + 1000000007 - dp[z][o-limit-1][0]) % 1000000007;
+                }
+            }
+        }
+        return (dp[zero][one][0] + dp[zero][one][1]) % 1000000007;
     }
 };
