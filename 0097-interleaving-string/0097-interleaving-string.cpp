df535e38d1ad96491910fea4ca35bd4c6986877c
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-08-06 23:36:56 +0530

    Time: 2 ms (64.97%), Space: 9.6 MB (48.19%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
+bool solve(vector<vector<int>> &dp , string &s1 , string &s2 , string &s3 , int i , int j , int k) {
+    if(i == s1.size() && j == s2.size() && k == s3.size()) return true ;
+     if(i == s1.size()) {
+        while(j < s2.size()) {
+            if(s2[j++] != s3[k++]) return false ;
+        }
+        return true ;
+    }
+     if(j == s2.size()) {
+        while(i < s1.size()) {
+            if(s1[i++] != s3[k++]) return false ;
+        }
+        return true ;
+    }
+
+    if(dp[i][j] != -1) return dp[i][j] ;
+     
+   bool take = false ;
+
+    if(s2[j] == s3[k]) take |= solve(dp ,s1 , s2 , s3 , i , j + 1 , k + 1) ;
+    if(s1[i] == s3[k]) take |= solve(dp , s1 , s2 , s3 , i + 1 , j , k + 1) ;
+    
+    return dp[i][j] = take ;
+}
+
     bool isInterleave(string s1, string s2, string s3) {
-        int m = s1.length(), n = s2.length(), l = s3.length();
-        if (m + n != l) return false;
-        
-        if (m < n) return isInterleave(s2, s1, s3);
+    if(s1.size() + s2.size() != s3.size()) return false ;
 
-        vector<bool> dp(n + 1, false);
-        dp[0] = true;
+    vector<vector<int>> dp(s1.size() , vector<int> (s2.size() , -1)) ;
 
-        for (int j = 1; j <= n; ++j) {
-            dp[j] = dp[j - 1] && s2[j - 1] == s3[j - 1];
-        }
-
-        for (int i = 1; i <= m; ++i) {
-            dp[0] = dp[0] && s1[i - 1] == s3[i - 1];
-            for (int j = 1; j <= n; ++j) {
-                dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
-            }
-        }
-        
-        return dp[n];
+    return solve(dp ,s1 , s2 , s3 , 0 , 0 , 0) ; 
     }
 };
