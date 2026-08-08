e21e19be136a4ff5a29135dd4b8a266fcc8d7512
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-08-09 01:59:04 +0530

    Time: 3 ms (69.11%), Space: 15.6 MB (64.12%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void solve(int open, int close, int n, string &temp, vector<string> &ans) {
-        if (temp.size() == 2 * n) {
-            ans.push_back(temp);
-            return;
-        }
-        if (open < n) {
-            temp.push_back('(');
-            solve(open + 1, close, n, temp, ans);
-            temp.pop_back();
-        }
-        if (close < open) {
-            temp.push_back(')');
-            solve(open, close + 1, n, temp, ans);
-            temp.pop_back();
-        }
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        backtrack("", 0, 0, n, result);
+        return result;
     }
 
-    vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        string temp;
-        solve(0, 0, n, temp, ans);
-        return ans;
+    void backtrack(string current, int open, int close, int n,
+                   vector<string>& result) {
+        
+        if (current.size() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(current + "(", open + 1, close, n, result);
+        }
+
+        if (close < open) {
+            backtrack(current + ")", open, close + 1, n, result);
+        }
     }
 };
