528d7af6a3f0fd82eb41c6126eaf0789a4a437cc
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-08-22 02:53:10 +0530

    Time: 0 ms (100%), Space: 8.9 MB (17.83%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string getPermutation(int n, int k) {
         vector<int> numbers;
-        int fact = 1; 
+        int fact = 1;
 
         for(int i=1; i<n; i++){
             fact = fact * i;
@@ -14,14 +14,18 @@ public:
         k = k-1;
 
         while(1){
+            // to select the fixed ith index
             ans = ans + to_string(numbers[k/fact]);
 
+            //delete the chosen number
             numbers.erase(numbers.begin() + k/fact);
 
+            //break the loop if nothing is left in numbers
             if(numbers.size()==0){
                 break;
             }
 
+            //to get next iteration k, fact
             k = k%fact;
             fact= fact / numbers.size();
         }
