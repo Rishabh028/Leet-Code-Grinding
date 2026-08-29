fdc704d06da3452a7f058c6f312c11869a452f7b
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-08-30 00:21:27 +0530

    Time: 3 ms (17.96%), Space: 9 MB (56.95%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,21 +1,30 @@
 class Solution {
- public:
-  string addBinary(string a, string b) {
-    string ans;
-    int carry = 0;
-    int i = a.length() - 1;
-    int j = b.length() - 1;
+public:
+    string addBinary(string a, string b) {
+        if (a.size() < b.size()) {
+            return addBinary(b, a);
+        }
+        
+        int it_a = a.size() - 1;
+        int it_b = b.size() - 1;
+        int carry = 0;
+        string ans = "";
+        
+        while (it_a >= 0) {
+            if (a[it_a--] == '1') {
+                carry++;
+            }
+            if (it_b >= 0 && b[it_b--] == '1') {
+                carry++;
+            }
+            ans.push_back((carry % 2) ? '1' : '0');
+            carry /= 2;
+        }
+        if (carry == 1) {
+            ans.push_back('1');
+        }
 
-    while (i >= 0 || j >= 0 || carry) {
-      if (i >= 0)
-        carry += a[i--] - '0';
-      if (j >= 0)
-        carry += b[j--] - '0';
-      ans += carry % 2 + '0';
-      carry /= 2;
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
-
-    reverse(begin(ans), end(ans));
-    return ans;
-  }
 };
