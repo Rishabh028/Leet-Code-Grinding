e51c752323d446f1c15f6ed2acfa439d48b0a4ae
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-02 02:02:33 +0530

    Time: 0 ms (100%), Space: 10.9 MB (83.84%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        vector<string> components;
-        stringstream ss(path);
-        string comp;
-        while (getline(ss, comp, '/')) {
-            if (comp == "" || comp == ".") {
-                continue;
-            }
-
-            if (comp == "..") {
-                if (!st.empty()) {
-                    st.pop_back();
+        vector<string> p(1, "");
+        int i = 0, n = path.size();
+        while (i <= n) {
+            if (i == n || path[i] == '/') {
+                if (p.back() == ".") {
+                    p.back() = "";
                 }
-            } else {
-                st.push_back(comp);
+                else if (p.back() == "..") {
+                    p.pop_back();
+                    if (p.size())
+                        p.back() = "";
+                    else
+                        p.push_back("");
+                }
+                else if (p.back().size() > 0) 
+                    p.push_back("");
             }
+            else 
+                p.back() += path[i];;
+            ++i;
         }
-
-        stringstream simplifiedPath;
-        for (const string& s : st) {
-            simplifiedPath << "/" << s;
-        }
-
-        return simplifiedPath.str().empty() ? "/" : simplifiedPath.str();        
+        if (p[0] == "")
+            return "/";
+        string res;
+        for (int i = 0; i < p.size() - 1; ++i)
+            res += "/" + p[i];
+        return res;
     }
-
-private:
-    vector<string> st;    
 };
