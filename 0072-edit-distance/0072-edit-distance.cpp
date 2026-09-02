cddbd9f610f11a218cd589f4628dde9fc69ca128
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-03 02:32:52 +0530

    Time: 7 ms (61.29%), Space: 10.5 MB (93.71%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,36 @@
 class Solution {
- public:
-  int minDistance(string word1, string word2) {
-    const int m = word1.length();//first word length
-    const int n = word2.length();//second word length
-    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-
-    for (int i = 1; i <= m; ++i)
-      dp[i][0] = i;
-
-    for (int j = 1; j <= n; ++j)
-      dp[0][j] = j;
-
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        if (word1[i - 1] == word2[j - 1])//same characters
-          dp[i][j] = dp[i - 1][j - 1];//no operation
-        else
-          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
-                             //replace       //delete        //insert
-    return dp[m][n];
-  }
+public:
+    int minDistance(std::string word1, std::string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        // Keep word2 as the shorter string to optimize space usage
+        if (m < n) {
+            return minDistance(word2, word1);
+        }
+        
+        // dp array stores the edit distances for the previous row
+        std::vector<int> dp(n + 1);
+        for (int j = 0; j <= n; j++) {
+            dp[j] = j;
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            int prev = dp[0];
+            dp[0] = i;  // Base case for converting word1[:i] to an empty string
+            
+            for (int j = 1; j <= n; j++) {
+                int temp = dp[j];
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[j] = prev;  // Characters match, no operation needed
+                } else {
+                    // Choose minimum operation among: Delete, Insert, Replace
+                    dp[j] = 1 + std::min({dp[j], dp[j - 1], prev});
+                }
+                prev = temp;
+            }
+        }
+        
+        return dp[n];
+    }
 };
