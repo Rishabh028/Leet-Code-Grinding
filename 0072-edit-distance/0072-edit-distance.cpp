ce8b0bc57337c5ee06b34d05379a4461d9b39058
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-03 02:32:34 +0530

    Time: 3 ms (91.51%), Space: 13.1 MB (61.46%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,24 @@
 class Solution {
-public:
-    int minDistance(string word1, string word2) {
-        int m = word1.size();
-        int n = word2.size();
-        vector<vector<int>> cache(m + 1, vector<int>(n + 1, -1));
-        return dp(word1, word2, m, n, cache);
-    }
+ public:
+  int minDistance(string word1, string word2) {
+    const int m = word1.length();//first word length
+    const int n = word2.length();//second word length
+    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
 
-private:
-    int dp(const string& word1, const string& word2, int i, int j, vector<vector<int>>& cache) {
-        if (i == 0) return j;
-        if (j == 0) return i;
-        if (cache[i][j] != -1) return cache[i][j];
+    for (int i = 1; i <= m; ++i)
+      dp[i][0] = i;
 
-        if (word1[i - 1] == word2[j - 1]) {
-            cache[i][j] = dp(word1, word2, i - 1, j - 1, cache);
-        } else {
-            cache[i][j] = 1 + min({
-                dp(word1, word2, i - 1, j, cache),     
-                dp(word1, word2, i, j - 1, cache),    
-                dp(word1, word2, i - 1, j - 1, cache)  
-            });
-        }
+    for (int j = 1; j <= n; ++j)
+      dp[0][j] = j;
 
-        return cache[i][j];
-    }
+    for (int i = 1; i <= m; ++i)
+      for (int j = 1; j <= n; ++j)
+        if (word1[i - 1] == word2[j - 1])//same characters
+          dp[i][j] = dp[i - 1][j - 1];//no operation
+        else
+          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
+                             //replace       //delete        //insert
+    return dp[m][n];
+  }
 };
