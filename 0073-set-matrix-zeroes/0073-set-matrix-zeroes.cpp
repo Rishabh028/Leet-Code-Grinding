24dd64f97fc82b9123f9a6837c73ccaf024814d9
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-06 02:10:19 +0530

    Time: 0 ms (100%), Space: 20.9 MB (73.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
     int n = matrix.size();
-        if (n == 0) return; // Edge case for an empty matrix
+        if (n == 0) return; 
         int m = matrix[0].size();
     int col0 = 1;
-    // step 1: Traverse the matrix and
-    // mark 1st row & col accordingly:
+   
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (matrix[i][j] == 0) {
-                // mark i-th row:
                 matrix[i][0] = 0;
 
-                // mark j-th column:
                 if (j != 0)
                     matrix[0][j] = 0;
                 else
@@ -22,11 +19,9 @@ public:
         }
     }
 
-    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
     for (int i = 1; i < n; i++) {
         for (int j = 1; j < m; j++) {
             if (matrix[i][j] != 0) {
-                // check for col & row:
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
@@ -34,7 +29,6 @@ public:
         }
     }
 
-    //step 3: Finally mark the 1st col & then 1st row:
     if (matrix[0][0] == 0) {
         for (int j = 0; j < m; j++) {
             matrix[0][j] = 0;
