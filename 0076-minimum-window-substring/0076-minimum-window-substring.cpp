52a8e6483af4205e00fb6fb48b09d3375826a63b
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-08 22:01:09 +0530

    Time: 10 ms (49.98%), Space: 11.3 MB (82.96%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,47 +1,26 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.length() < t.length()) {
-            return "";
-        }
-
-        unordered_map<char, int> charCount;
-        for (char ch : t) {
-            charCount[ch]++;
-        }
-
-        int targetCharsRemaining = t.length();
-        int minWindow[2] = {0, INT_MAX};
-        int startIndex = 0;
-
-        for (int endIndex = 0; endIndex < s.length(); endIndex++) {
-            char ch = s[endIndex];
-            if (charCount.find(ch) != charCount.end() && charCount[ch] > 0) {
-                targetCharsRemaining--;
-            }
-            charCount[ch]--;
-
-            if (targetCharsRemaining == 0) {
-                while (true) {
-                    char charAtStart = s[startIndex];
-                    if (charCount.find(charAtStart) != charCount.end() && charCount[charAtStart] == 0) {
-                        break;
-                    }
-                    charCount[charAtStart]++;
-                    startIndex++;
+        unordered_map<char,int> frq;
+        int cnt=0, stIdx=-1, minlen=INT_MAX ;
+        if(s.size()<t.size()) return "";
+        for(int i=0;i<t.size();i++) frq[t[i]]++;
+        int l=0,r=0;
+        while(r<s.size()){
+            
+            if(frq[s[r]]>0) cnt++;
+            frq[s[r]]--;
+            while(cnt == t.size()) {
+                if(r-l+1 < minlen){
+                    minlen = r-l+1;
+                    stIdx = l;
                 }
-
-                if (endIndex - startIndex < minWindow[1] - minWindow[0]) {
-                    minWindow[0] = startIndex;
-                    minWindow[1] = endIndex;
-                }
-
-                charCount[s[startIndex]]++;
-                targetCharsRemaining++;
-                startIndex++;
+                frq[s[l]]++;
+                if(frq[s[l]]>0) cnt--;
+                l++;
             }
+            r++;
         }
-
-        return minWindow[1] >= s.length() ? "" : s.substr(minWindow[0], minWindow[1] - minWindow[0] + 1);        
+        return minlen == INT_MAX ? "" : s.substr(stIdx,minlen);
     }
 };
