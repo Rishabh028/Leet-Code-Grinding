d86b66cf19ba3918e930d44f13d5e45cea58af18
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-12 14:21:42 +0530

    Time: 3 ms (2.96%), Space: 11.2 MB (9.22%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,23 +10,34 @@
  * };
  */
 class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        stack<TreeNode*> st;
-        TreeNode* n = root;
-        while(st.size()> 0 || n){
-            if(n != NULL) {
-                st.push(n);
-                n = n -> left;
+private:
+    vector<int>ino(TreeNode* root){
+        vector<int>post,pre,in;
+        if(root==NULL) return post;
+        stack<pair<TreeNode*,int>>st;
+        st.push({root,1});
+        while(!st.empty()){
+            auto it=st.top();
+            st.pop();
+            if(it.second==1){
+                pre.push_back(it.first->val);
+                st.push({it.first,2});
+                if(it.first->left) st.push({it.first->left,1});
             }
-            else {
-                TreeNode* temp = st.top();
-                st.pop();
-                ans.push_back(temp -> val);
-                n = temp -> right;
+            else if(it.second==2){
+                in.push_back(it.first->val);
+                st.push({it.first,3});
+                if(it.first->right) st.push({it.first->right,1});
+            }
+            else{
+                post.push_back(it.first->val);
             }
         }
+        return in;
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans=ino(root);
         return ans;
     }
 };
