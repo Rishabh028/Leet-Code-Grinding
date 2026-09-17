1df2df4cdc1a051099444935038c2e97c4768c8f
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-17 23:34:33 +0530

    Time: 0 ms (100%), Space: 18 MB (9.52%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,26 +11,20 @@
  */
 class Solution {
 public:
-    void flatten(TreeNode* root) 
-    {
-     TreeNode * curr = root ; 
-     
-     while(curr)
-     {
-        if(curr->left != nullptr)
-        {
-          TreeNode * prev = curr->left ;  
-          while(prev->right)
-          {
-            prev = prev->right ;
-          }
-        
-          prev->right = curr->right ;
-          curr->right = curr->left ; 
-          curr->left = nullptr ; 
+    void flatten(TreeNode* root) {
+        if (!root) return;
+        vector<TreeNode*> nodes;
+        preorder(root, nodes);
+        for (int i = 0; i < nodes.size() - 1; i++) {
+            nodes[i]->left = nullptr;
+            nodes[i]->right = nodes[i + 1];
         }
-        curr = curr->right ;
-     }
-    
+    }
+
+    void preorder(TreeNode* node, vector<TreeNode*>& nodes) {
+        if (!node) return;
+        nodes.push_back(node);
+        preorder(node->left, nodes);
+        preorder(node->right, nodes);
     }
 };
