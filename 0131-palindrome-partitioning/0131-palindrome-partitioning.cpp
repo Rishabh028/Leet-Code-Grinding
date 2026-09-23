9022890be8d8691fee27a5d15e66eb8b58daad26
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-23 15:00:50 +0530

    Time: 27 ms (94.7%), Space: 53.2 MB (98.68%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,31 +1,46 @@
+ 
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> path;
-        backtrack(s, 0, path, result);
-        return result;
-    }
-private:
-    void backtrack(const string s, int start, vector<string>& path, vector<vector<string>>& result) {
-        if (start == s.length()) {
-            result.push_back(path);
-            return;
-        }
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (isPalindrome(s, start, end - 1)) {
-                path.push_back(s.substr(start, end - start));
-                backtrack(s, end, path, result);
-                path.pop_back();
-            }
-        }
-    }
-    bool isPalindrome(const string& s, int left, int right) {
-        while (left < right) {
-            if (s[left++] != s[right--]) {
+   
+    bool isPalindrome(const string& str, int start, int end) {
+        while (start < end) {
+            if (str[start] != str[end]) {
                 return false;
             }
+            start++;
+            end--;
         }
         return true;
+    }
+
+  
+    void generate(int ind, vector<string>& cp, vector<vector<string>>& result, const string& s) {
+        
+        if (ind == s.length()) {
+            result.push_back(cp);
+            return;
+        }
+
+      
+        for (int i = ind; i < s.length(); i++) {
+            
+            if (isPalindrome(s, ind, i)) {
+                
+                cp.push_back(s.substr(ind, i - ind + 1));
+                
+             
+                generate(i + 1, cp, result, s);
+                
+                
+                cp.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> result;
+        vector<string> cp;
+        generate(0, cp, result, s);
+        return result;
     }
 };
