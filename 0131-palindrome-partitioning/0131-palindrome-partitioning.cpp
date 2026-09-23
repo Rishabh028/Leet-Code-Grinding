52578624e5424b92d935584ec389a8f7d700ea8a
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-23 15:00:13 +0530

    Time: 47 ms (53.4%), Space: 57.7 MB (52.2%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -6,14 +6,13 @@ public:
         backtrack(s, 0, path, result);
         return result;
     }
-
 private:
-    void backtrack(const string& s, int start, vector<string>& path, vector<vector<string>>& result) {
+    void backtrack(const string s, int start, vector<string>& path, vector<vector<string>>& result) {
         if (start == s.length()) {
             result.push_back(path);
             return;
         }
-        for (int end = start + 1; end <= s.length(); ++end) {
+        for (int end = start + 1; end <= s.length(); end++) {
             if (isPalindrome(s, start, end - 1)) {
                 path.push_back(s.substr(start, end - start));
                 backtrack(s, end, path, result);
@@ -21,7 +20,6 @@ private:
             }
         }
     }
-
     bool isPalindrome(const string& s, int left, int right) {
         while (left < right) {
             if (s[left++] != s[right--]) {
