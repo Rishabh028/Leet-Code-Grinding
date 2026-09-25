d64d339d467c2a5587fdc341a793638ede15064d
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-25 16:40:34 +0530

    Time: 8 ms (80.69%), Space: 11.9 MB (21.4%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -15,7 +15,6 @@ public:
         while (fast != nullptr && fast -> next != nullptr) {
             slow = slow -> next;
             fast = fast -> next -> next;
-
             if (slow == fast) {
                 return true;
             }
