5f539a0e3ae5f0a04e45edaddd993eded9b32fe9
Rishabh Rajak <161935032+Rishabh028@users.noreply.github.com>
2026-09-26 18:14:08 +0530

    Time: 0 ms (100%), Space: 22.8 MB (51.33%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,44 +1,37 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if (!head) return;
+        if (!head || !head->next || !head->next->next) return;
 
         ListNode* slow = head;
         ListNode* fast = head;
-        while (fast && fast -> next) {
-            slow = slow -> next;
-            fast = fast -> next -> next;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode* second = slow -> next;
-        slow -> next = nullptr;
-        ListNode* node = nullptr;
 
-        while (second) {
-            ListNode* temp = second -> next;
-            second -> next = node;
-            node = second;
-            second = temp;
+        ListNode* prev = nullptr;
+        ListNode* curr = slow->next;
+        slow->next = nullptr; 
+
+        while (curr) {
+            ListNode* next_node = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next_node;
         }
+
         ListNode* first = head;
-        second = node;
-
+        ListNode* second = prev;
         while (second) {
-            ListNode* temp1 = first -> next;
-            ListNode* temp2 = second -> next;
-            first -> next = second;
-            second ->next = temp1;
-            first = temp1;
-            second = temp2;
+            ListNode* tmp1 = first->next;
+            ListNode* tmp2 = second->next;
+
+            first->next = second;
+            second->next = tmp1;
+
+            first = tmp1;
+            second = tmp2;
         }
     }
 };
